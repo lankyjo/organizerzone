39f3ae484067ed8b769bdb591a28b9c7b2940a7a
chile4coding <chileomereji@gmail.com>
2025-04-01 07:11:46 +0100

    worked on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 import Hero from "@/components/Hero/Hero";
 import Nav from "@/components/Navigation/Nav";
@@ -15,18 +14,3 @@ export default function Home() {
     </>
   );
 }
-=======
-import Hero from "@/components/Hero";
-import Nav from "@/components/Nav";
-
-export default function Home(){
-  return(
-    <>
-    <main className="min-h-[2000px]">
-    <Nav/>
-    <Hero/>
-    </main>
-    </>
-  )
-}
->>>>>>> 77f60b76eae6e13ac7c9b34f2a6478949b1af4f9
